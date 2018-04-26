0d80e1adfc8b51d7a5e560050a5b0b738554df88
Václav Lovas <vaclav.lovas@seznam.cz>
2018-04-26 14:07:26 +0200

    init commit

---
+++ asteroidy.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 26 13:27:57 2018
+
+@author: lov35174
+"""
+
+import pyglet
+
+window = pyglet.window.Window()
+pyglet.app.run()
+print('Hotovo!')
